43f0f7b9e5b201e1a41e07111e5f8daa0488fea0
Cassidy777 <Cassidy.take@gmail.com>
2025-01-08 10:49:58 +0900

    change locations

--- app/routes/locations.py
+++ app/routes/locations.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import Prefecture
+from typing import List
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("", response_model=List[Prefecture])
 def get_locations(db: Session = Depends(get_db)):
     """
     都道府県リストを取得するエンドポイント
